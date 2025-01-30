c99088f84c08444c486de30a2aeef23c56e6d236
Mehul Kurhe <143060203+Mehul0906@users.noreply.github.com>
2025-01-30 18:18:59 +0530

    Add files via upload

---
+++ Day-1 IF, IF ELSE & LADDER IF ELSE/NUMBER IS NEGATIVE, POSITIVE, OR NEUTRAL.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std; 
+int main(){
+	int number;
+	cout <<"Enetr your Number :-";
+	cin >> number;
+	if(number > 0)
+	{
+	cout <<	"The Number is Positive";  
+	}
+	else if(number < 0)
+	{
+		cout << "The Number Negative" ; 
+	}
+	else{
+		cout << "The Number Is Nautral"; 
+	}
+	
+}
+
